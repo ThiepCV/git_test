b80bdc10f3a3a90a9733c62e65a92ed1bf66dfa4
ThiepCV <thiepcv@vnext.vn>
2024-09-23 11:48:16 +0900

    dung thu -a

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          dong dau tien duoc them vao
         </p>
+        <p>dong thu 2 </p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/component/home.js
@@ -0,0 +1,7 @@
+const home =() =>{
+    return(
+        <div>
+            <h1>Day la home</h1>
+        </div>
+    )
+}
